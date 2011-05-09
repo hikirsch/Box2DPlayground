9b3e7e978c4594bfc26b4b14d1a6eaaf25b98f81
Adam S. Kirschner <accounts@adamskirschner.com>
2011-05-09 02:42:00 -0400

    it works, need to figure out the walls problem

---
+++ Box2DTester.js
@@ -0,0 +1,170 @@
+function Box2DTester(canvasId) {
+	this._fps = 200;
+	this._velocityIterationsPerSecond = 100;
+	this._positionIterationsPerSecond = 200;
+
+	this.initCanvas(canvasId);
+	this.createWorld();
+	this.initCAAT();
+}
+
+Box2DTester.prototype = {
+	initCanvas: function( canvasId ) {
+		this._canvasEle = document.getElementById(canvasId);
+		
+		Site.Constants.GAME_WIDTH = this._canvasEle.clientWidth;
+		Site.Constants.GAME_HEIGHT = this._canvasEle.clientHeight;
+	},
+	
+	createWorld: function() {
+		this._world = new b2World(new b2Vec2(0, 10), true);
+		this._world.SetWarmStarting(true);
+
+		// Create border of boxes
+		var wall = new b2PolygonShape(),
+			wallBd = new b2BodyDef();
+
+		// Left
+		var dimensions = new b2Vec2( 1 / Site.Constants.PHYSICS_SCALE, Site.Constants.GAME_HEIGHT / Site.Constants.PHYSICS_SCALE );
+		wallBd.position.Set( 0, 0 );
+		wall.SetAsBox( dimensions.x, dimensions.y );
+
+		this._wallLeft = this._world.CreateBody(wallBd);
+		this._wallLeft.w = dimensions.x;
+		this._wallLeft.h = dimensions.y;
+		this._wallLeft.CreateFixture2(wall);
+		this._wallLeft.EntityID = Site.GetNextEntityID();
+
+		// Right
+		wallBd.position.Set( ( Site.Constants.GAME_WIDTH - 1 ) / Site.Constants.PHYSICS_SCALE, 0  );
+		wall.SetAsBox( dimensions.x, dimensions.y );
+
+		this._wallRight = this._world.CreateBody(wallBd);
+		this._wallRight.w = dimensions.x;
+		this._wallRight.h = dimensions.y;
+		this._wallRight.CreateFixture2(wall);
+		this._wallRight.EntityID = Site.GetNextEntityID();
+
+		// BOTTOM
+		dimensions = new b2Vec2( Site.Constants.GAME_WIDTH / Site.Constants.PHYSICS_SCALE, 1 / Site.Constants.PHYSICS_SCALE );
+		wallBd.position.Set( 0, ( Site.Constants.GAME_HEIGHT - 1 ) / Site.Constants.PHYSICS_SCALE );
+		wall.SetAsBox( dimensions.x, dimensions.y );
+
+		this._wallBottom = this._world.CreateBody(wallBd);
+		this._wallBottom.w = Site.Constants.GAME_WIDTH/ Site.Constants.PHYSICS_SCALE;
+		this._wallBottom.h = 1 / Site.Constants.PHYSICS_SCALE;
+		this._wallBottom.CreateFixture2(wall);
+		this._wallBottom.EntityID = Site.GetNextEntityID();
+
+		this._wallBottom._color = this._wallLeft._color = this._wallRight._color = "#ff0000";
+	},
+
+	update: function() {
+		// derive passed time since last update. max. 10 secs
+		var time = new Date().getTime(),
+			delta = (time - this._lastUpdate) / 1000;
+
+		this._lastUpdate = time;
+
+		if(delta > 10) { delta = 1/this._fps; }
+
+		this.step(delta);
+		this.draw();
+	},
+
+	step: function(delta) {
+		if( ! this._world ) { return; }
+
+		this._world.ClearForces();
+
+		delta = (typeof delta == "undefined")
+			? 1/this._fps
+			: delta;
+
+		this._world.Step(delta, delta * this._velocityIterationsPerSecond, delta * this._positionIterationsPerSecond);
+	},
+
+	draw: function() {
+		var nextBox2DBody = this._world.m_bodyList;
+
+		while( nextBox2DBody && "EntityID" in nextBox2DBody ) {
+			if( nextBox2DBody.m_userData == null ) {
+				this.initCAATShapeFromBox2DBody( nextBox2DBody );
+			}
+
+			var newPosition = nextBox2DBody.GetPosition();
+
+			nextBox2DBody.m_userData.setLocation(
+				( newPosition.x - ( nextBox2DBody.w / 2 ) ) * Site.Constants.PHYSICS_SCALE >> 0,
+				( newPosition.y - ( nextBox2DBody.h / 2 ) ) * Site.Constants.PHYSICS_SCALE >> 0
+			);
+
+			nextBox2DBody.m_userData.setRotation( nextBox2DBody.GetAngle() );
+
+			nextBox2DBody = nextBox2DBody.m_next;
+		}
+	},
+
+	createBoxes: function( count ) {
+		for(var i = 0; i < count; i ++) {
+			this.spawn(Site.Constants.BOX_WIDTH);
+		}
+	},
+
+	spawn: function( boxSize ) {
+		var nextID = Site.GetNextEntityID(),
+			x = ( Math.random() * Site.Constants.GAME_WIDTH ) - boxSize,
+			y = -( nextID * boxSize / 2 );
+
+		var bodyDef = new b2BodyDef();
+		bodyDef.type = b2Body.b2_dynamicBody;
+		bodyDef.position.Set(
+			x / Site.Constants.PHYSICS_SCALE,
+			y / Site.Constants.PHYSICS_SCALE
+		);
+
+		var body = this._world.CreateBody(bodyDef);
+		body.w = boxSize / Site.Constants.PHYSICS_SCALE;
+		body.h = boxSize / Site.Constants.PHYSICS_SCALE;
+		body.EntityID = nextID;
+		body._color = '#' + CAAT.Color.prototype.hsvToRgb( ( body.EntityID * 15 ) % 360, 40, 99 ).toHex();
+
+		var shape = new b2PolygonShape.AsBox(body.w / 2, body.h / 2);
+
+		var fixtureDef = new b2FixtureDef();
+		fixtureDef.restitution = 0.1;
+		fixtureDef.density = 1.0;
+		fixtureDef.friction = 0.2;
+		fixtureDef.shape = shape;
+
+		body.CreateFixture(fixtureDef);
+
+		return body;
+	},
+
+	initCAAT: function( canvasId ) {
+		var director = new CAAT.Director().initialize( Site.Constants.GAME_WIDTH, Site.Constants.GAME_HEIGHT, this._canvasEle ),
+			that = this;
+
+		this._scene = new CAAT.Scene().create();
+		this._scene.setFillStyle("#000000");
+
+		director.addScene(this._scene);
+		director.loop( 30, function() {
+			that.update();
+		});
+	},
+
+	initCAATShapeFromBox2DBody: function( box2DBody ) {
+		box2DBody.m_userData = new CAAT.ShapeActor()
+			.setShape( CAAT.ShapeActor.prototype.SHAPE_RECTANGLE )
+			.create()
+			.setSize(
+				box2DBody.w * Site.Constants.PHYSICS_SCALE,
+				box2DBody.h * Site.Constants.PHYSICS_SCALE
+			)
+			.setFillStyle( box2DBody._color );
+
+		this._scene.addChild( box2DBody.m_userData );
+	}
+};
--- Site.js
+++ Site.js
@@ -1,96 +1,29 @@
-var DemoBox2D = {
+var Site = {
+	_NEXT_ENTITY_ID: 0,
+	
 	Constants: {
-		GAME_HEIGHT: 600,
-		GAME_WIDTH: 800,
-		PHYSICS_SCALE: 32
-	}
-};
-
-var Site = {};
-
-Site.InitStats= function() {
-	var stats = new Stats();
-
-	document.getElementById("stats").appendChild( stats.domElement );
-
-	setInterval( function () { stats.update(); }, 1000 / 60 );
-};
-
-Site.Loop = function() {
-	if( Site.Box2DEnv == null || Site.Box2DEnv._world == null ) { return; }
-
-	var nextBox2DBody = Site.Box2DEnv._world.m_bodyList;
-
-	while( nextBox2DBody ) {
-		if( nextBox2DBody != null && nextBox2DBody.m_userData != null ) {
-			var newPosition = nextBox2DBody.GetPosition();
-			
-			nextBox2DBody.m_userData.setLocation(
-				newPosition.x * DemoBox2D.Constants.PHYSICS_SCALE >> 0,
-				newPosition.y * DemoBox2D.Constants.PHYSICS_SCALE >> 0
-			);
-			
-			nextBox2DBody.m_userData.setRotation( nextBox2DBody.GetAngle() );
-			
-//			console.log( nextBox2DBody.ID, newPosition.x * DemoBox2D.Constants.PHYSICS_SCALE >> 0, newPosition.y * DemoBox2D.Constants.PHYSICS_SCALE >> 0)
-		}
-		nextBox2DBody = nextBox2DBody.m_next;
+		PHYSICS_SCALE: 32,
+		BOX_WIDTH: 30,
+		BOX_COUNT: 100
 	}
 };
 
 Site.Init = function() {
-	var height = $("#canvas").height(),
-		width = $("#canvas").width();
-
-	Site.InitCAAT( height, width );
-
-	var canvas = document.getElementById("canvas");
-	Site.Box2DEnv = new Box2DTester( canvas );
-
-	Site.Box2DEnv.draw();
-	Site.Box2DEnv.resume();
 	Site.InitStats();
+
+	var tester = new Box2DTester("canvas");
+	tester.createBoxes( Site.Constants.BOX_COUNT );
 };
 
-Site.InitCAAT = function( height, width ) {
-	var director = new CAAT.Director().initialize(
-		width,    // 100 pixels wide
-		height,    // 100 pixels across
-		document.getElementById('canvas')
-	);
-
-	Site._scene = new CAAT.Scene().create();
-	Site._scene.setFillStyle("#000000");
-
-	director.addScene(Site._scene);
-	director.loop( 60, Site.Loop );
+Site.InitStats = function() {
+	var stats = new Stats();
+	document.getElementById("stats").appendChild( stats.domElement );
+	setInterval( function () { stats.update(); }, 1000 / 60 );
 };
 
-Site.ID = 0;
-
-Site.InitCaatFromBox2DLoop = function( box2DBody ) {
-	var position = box2DBody.GetPosition();
-	console.log( box2DBody.ID,
-		position.x * DemoBox2D.Constants.PHYSICS_SCALE,
-		position.y * DemoBox2D.Constants.PHYSICS_SCALE,
-		box2DBody.w * DemoBox2D.Constants.PHYSICS_SCALE,
-		box2DBody.h * DemoBox2D.Constants.PHYSICS_SCALE
-	);
-
-	box2DBody.m_userData = new CAAT.ShapeActor()
-		.setShape( CAAT.ShapeActor.prototype.SHAPE_RECTANGLE )
-		.create()
-		.setLocation(
-			position.x * DemoBox2D.Constants.PHYSICS_SCALE,
-			position.y * DemoBox2D.Constants.PHYSICS_SCALE
-		)
-		.setSize(
-			box2DBody.w * DemoBox2D.Constants.PHYSICS_SCALE * 2,
-			box2DBody.h * DemoBox2D.Constants.PHYSICS_SCALE * 2
-		)
-		.setFillStyle( box2DBody._color );
-
-	Site._scene.addChild( box2DBody.m_userData );
+Site.GetNextEntityID = function() {
+	Site._NEXT_ENTITY_ID++;
+	return Site._NEXT_ENTITY_ID;
 };
 
-$( Site.Init );
+domReady( Site.Init );
